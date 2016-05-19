47b28ca53ae64532153ae76782647c23d2e9db7b
Mahesh <mbprajapati22@gmail.com>
2016-05-19 17:22:24 +0530

    added angular module

--- script/app.js
+++ script/app.js
@@ -1,3 +1,13 @@
 /**
  * Created by mahesh on 18/5/16.
  */
+
+var angularApp = angular.module("angularApp",[]);
+
+angularApp.controller("ArticleListController",['$scope','$http', function($scope,$http) {
+    //getArticleList();
+    function getArticleList() {
+        $http.post('data/response.json').success(function (data) {
+            $scope.details = data;
+        });
+    });
