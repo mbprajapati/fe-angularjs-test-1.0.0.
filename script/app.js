16c7172bc7bae7241ed197ad56fd25ddeac0bcbc
Mahesh Prajapati <mbprajapati22@gmail.com>
2016-05-24 15:11:48 +0530

    Changes to index page,angular controller and add main.css

--- script/app.js
+++ script/app.js
@@ -2,10 +2,33 @@
  * Created by mahesh on 18/5/16.
  */
 
+//header('content-type: application/json; charset=utf-8');
+//header("access-control-allow-origin: *");
+
 var articleApp = angular.module("articleApp",[]);
 
-articleApp.controller('ArticleCtrl', function ($scope, $http) {
-    $http.get('http://data/response.json').success(function(data) {
-        $scope.Data = Data;
+articleApp.controller('ArticleListCtrl', function ($scope, $http) {
+    $http.get('data/response.json').success(function(data) {
+        $scope.Data = data;
     });
 });
+
+articleApp.controller('ArticleDetailCtrl', function ($scope, $http) {
+    $http.get('data/response.json').success(function(data) {
+        $scope.Data = data;
+    });
+});
+
+/* Routing
+articleApp.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl : 'pages/index.html',
+                controller  : 'ArticalListController'
+            })
+            .when('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'ArticleDetailCtrl'
+            });
+    });
+*/
